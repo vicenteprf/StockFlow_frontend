003141d842d8484e305d472c09de981959fd4ca8
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-08-05 19:18:05 -0300

    feat: adicionar cliente http com interceptador de autenticacao

---
+++ src/services/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
